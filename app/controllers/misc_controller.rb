f93c6389db79d876772b6c04da97c5e54432756b
Mariana Cardozo <146393028+maricardozo-booth@users.noreply.github.com>
2023-11-01 16:08:30 +0000

    working eldest

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -5,14 +5,14 @@ class MiscController < ApplicationController
 
   def youngest
     dobs = Director.all.order({ :dob => :desc })
-    @the_youngest = dobs.at(0)
+    @the_youngest = dobs.first
     render({ :template => "misc_templates/youngest"})
   end
 
   def eldest
     dobs = Director.all.order({ :dob => :desc })
-    dobs_wo_nil = dobs.where.not({ :dob => "nil" })
-    @the_eldest = dobs_wo_nil.last
+    dobs_wo_nil = dobs.where.not ({ :dob => nil })
+ @the_eldest = dobs_wo_nil.last
     render({ :template => "misc_templates/eldest"})
   end
 
